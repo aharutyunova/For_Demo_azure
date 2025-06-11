d7ec259f9f1c39d9e5cf1d3c82b47594263b8682
Anna <an.harutyunova@gmail.com>
2025-06-11 23:08:05 +0400

    Add repo

---
+++ conftest.py
@@ -0,0 +1,50 @@
+from selenium import webdriver
+import pytest
+import logging
+import os
+from datetime import datetime
+import allure
+
+
+@pytest.fixture
+def get_driver():
+    god = None
+    driver = webdriver.Chrome()
+    yield driver
+    driver.quit()
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s [%(levelname)s] %(message)s',
+    filemode='w'
+)
+
+
+@pytest.fixture()
+def test_logger(request):
+    today_date = datetime.today().date()
+    os.makedirs(f"logs_{today_date}", exist_ok=True)
+    test_name = request.node.name
+    log_path = os.path.join(f"logs_{today_date}", test_name)
+    logger = logging.getLogger(test_name)
+    file_handler = logging.FileHandler(log_path, mode='w')
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    logger.setLevel(logging.INFO)
+    logger.info(f'{test_name} is started')
+    yield logger
+    logger.info(f'{test_name} is finished')
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item):
+    outcome = yield
+    result = outcome.get_result()
+    if result.when == "call":
+        if result.outcome == 'failed':
+            allure.attach(
+                item.funcargs.get("get_driver").get_screenshot_as_png(),
+                name=f"{item.name}_screen",
+                attachment_type=allure.attachment_type.PNG)
---
+++ tests/test_3.py
@@ -0,0 +1,10 @@
+import pytest
+
+
+def test_case_1(get_driver):
+    assert 1 == 2
+
+
+
+    
+
